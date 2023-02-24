f563f86745228fe0bceb2cd9ed9eaca8982886c3
Osmond Sedem <osmondsedem@gmail.com>
2023-02-24 23:56:20 +0000

    simple shell project

--- environ.c
+++ environ.c
@@ -6,7 +6,9 @@
  *          constant function prototype.
  * Return: Always 0
  */
+
 int _myenv(info_t *info)
+
 {
 	print_list_str(info->env);
 	return (0);
@@ -16,13 +18,16 @@ int _myenv(info_t *info)
  * _getenv - gets the value of an environ variable
  * @info: Structure containing potential arguments. Used to maintain
  * @name: env var name
- *
  * Return: the value
  */
+
 char *_getenv(info_t *info, const char *name)
+
 {
 	list_t *node = info->env;
+
 	char *p;
+
 	while (node)
 	{
 		p = starts_with(node->str, name);
